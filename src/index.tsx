806ee35ce1898e518f6f21782f3241ac27bc0f94
Felipe Marinho <github@felipemarinho.dev>
2023-08-21 21:34:30 -0700

    `index.ts` rename file

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
-
-export * from '../src/components/Button';
